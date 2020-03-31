d6be781d26813de04c842b80b3fe9aa255527924
Travis Pessetto <travis@pessetto.com>
2020-03-30 23:12:08 -0600

    Add Ko-fi button

--- index.php
+++ index.php
@@ -23,10 +23,15 @@ require_once("./langs/language.php");
   <div><?php echo Language::Instance()->check_version; ?> <span class="waiting"></span></div>
 </div>
 <div id="count">
-    <!-- hitwebcounter Code START -->
-    <a href="https://www.hitwebcounter.com" target="_blank">
-    <img src="https://hitwebcounter.com/counter/counter.php?page=7165217&style=0007&nbdigits=5&type=ip&initCount=0" title="Counter For Website Hitwebcounter" Alt="hitwebcounter.com"   border="0" >
-    </a>
+    <!-- Ko-fi DIV -->
+    <div>
+      <script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#00B3B3', 'E1E71JUPY');kofiwidget2.draw();</script> 
+    </div>
+    <div>
+      <a href="https://www.hitwebcounter.com" target="_blank">
+      <img src="https://hitwebcounter.com/counter/counter.php?page=7165217&style=0007&nbdigits=5&type=ip&initCount=0" title="Counter For Website Hitwebcounter" Alt="hitwebcounter.com"   border="0" >
+      </a>
+    </div>
 </div>   
 </body>
 </html>
