024789e48c4079ffd89996058a26b41884f53fe4
Travis Pessetto <travis@pessetto.com>
2020-04-18 23:47:28 -0600

    Adding selenium tests

--- tests/controllerTest.php
+++ tests/controllerTest.php
@@ -1,5 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
 
 final class ControllerTest extends TestCase
 {
@@ -9,9 +11,23 @@ final class ControllerTest extends TestCase
         $this->assertEquals(true,true);
     }
 
+    // Test to make sure the internal PHP server is up and running
     public function testPhpWebserverActive()
     {
         $result = file_get_contents("http://localhost:9000");
-        $this->assertNotFalse($result);
+        $this->assertNotFalse($result,"PHP Internal server does not appear to be started");
+    }
+
+    // Test to make sure Selenium is up and running
+    // Webdriver basics at https://github.com/php-webdriver/php-webdriver/blob/master/example.php
+    public function testSeleniumSetup()
+    {
+        $host = 'http://localhost:4444';
+        $capabilities = DesiredCapabilities::chrome();
+        $driver = RemoteWebDriver::create($host, $capabilities);
+        $driver->get("http://localhost:9000");
+        $title = strtolower($driver->getTitle());
+        $gotTitle = strpos($title,"updater") !== false;
+        $this->assertTrue($gotTitle,"Selenium driver does not appear to be started");
     }
 }
