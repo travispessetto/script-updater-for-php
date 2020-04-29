fd010dcc1a64691c948e7ca65bca76852383d66f
Travis Pessetto <travis@pessetto.com>
2020-04-28 22:05:07 -0600

    Adding a language not detected fix

--- src/langs/language.php
+++ src/langs/language.php
@@ -4,7 +4,11 @@ class Language
 {
   protected function __construct()
   {
-      $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+      $lang = "en";
+      if(is_set($_SERVER) && array_key_exists("HTTP_ACCEPT_LANGUAGE",$_SERVER))
+      {
+        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+      }
       $langFile = dirname(__FILE__)."/php/$lang.php";
       if(file_exists($langFile))
       {
